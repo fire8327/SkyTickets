de3d4c9879f22edb3b7fd6994a986957cb3611a4
fire8327 <114702513+fire8327@users.noreply.github.com>
2024-03-26 01:15:09 +0300

    create validation

--- formkit.config.js
+++ formkit.config.js
@@ -1,7 +1,9 @@
 import { defineFormKitConfig } from '@formkit/vue'
+import { ru } from '@formkit/i18n'
 
 export default defineFormKitConfig({
   // rules: {},
-  // locales: {},
+  locales: { ru },
+  locale: 'ru',
   // etc. 
 })
--- pages/index.vue
+++ pages/index.vue
@@ -16,16 +16,16 @@
     <div class="flex flex-col items-center text-center gap-4 w-full">
         <p class="text-4xl uppercase">Поиск <span class="text-[#7DCCFF]">бюджетных</span> вариантов авиапутешествий</p>
         <p class="text-2xl">поможем вам в выборе</p>
-        <FormKit :actions="false" type="form" form-class="flex max-lg:flex-col items-center gap-2.5 text-lg w-full">
+        <FormKit :actions="false" type="form" form-class="flex max-lg:flex-col items-start gap-2.5 text-lg w-full" :validation-messages="false">
             <div class="grid grid-cols-1 lg:grid-cols-4 gap-1 w-full">
-                <FormKit type="text" input-class="bg-[#2e3035] focus:outline-none px-4 py-4 max-lg:rounded-t-xl lg:rounded-l-xl" placeholder="Откуда"/>
-                <FormKit type="text" input-class="bg-[#2e3035] focus:outline-none px-4 py-4" placeholder="Куда"/>
-                <div class="flex items-center gap-1 lg:col-span-2">                    
-                    <FormKit type="date" input-class="bg-[#2e3035] focus:outline-none px-4 py-4 w-1/2 max-lg:rounded-bl-xl" placeholder="Когда"/>
-                    <FormKit type="select" placeholder="Тип билета" :options="['Эконом','Комфорт','Бизнес']" input-class="bg-[#2e3035] focus:outline-none px-4 h-full w-1/2 max-lg:rounded-br-xl lg:rounded-r-xl"/>
+                <FormKit validation="required" name="Откуда" type="text" message-class="text-[#E71616]" outer-class="w-full" input-class="bg-[#2e3035] focus:outline-none px-4 py-4 max-lg:rounded-t-xl lg:rounded-l-xl w-full" placeholder="Откуда"/>
+                <FormKit validation="required" type="text" name="Куда" message-class="text-[#E71616]" outer-class="w-full" input-class="bg-[#2e3035] focus:outline-none px-4 py-4 w-full" placeholder="Куда"/>
+                <div class="flex items-start gap-1 lg:col-span-2">                    
+                    <FormKit validation="required" type="date" name="Дата вылета" message-class="text-[#E71616]" outer-class="w-1/2" inner-class="h-full" wrapper-class="h-[60px]" input-class="bg-[#2e3035] focus:outline-none px-4 h-full w-full max-lg:rounded-bl-xl" placeholder="Когда"/>
+                    <FormKit validation="required" type="select" name="Тип билета" message-class="text-[#E71616]" outer-class="w-1/2" inner-class="h-full" wrapper-class="h-[60px]" placeholder="Тип билета" :options="['Эконом','Комфорт','Бизнес']" input-class="bg-[#2e3035] focus:outline-none px-4 h-full w-full max-lg:rounded-br-xl lg:rounded-r-xl"/>
                 </div>
             </div>
-            <FormKit type="submit" input-class="px-4 h-full rounded-xl bg-[#7DCCFF] max-lg:py-2">Найти</FormKit>
+            <FormKit type="submit" outer-class="h-[60px] max-lg:mx-auto" inner-class="h-full" wrapper-class="h-full" input-class="px-4 h-full rounded-xl bg-[#7DCCFF] max-lg:py-2 focus:outline-none">Найти</FormKit>
         </FormKit>
     </div>
     <div class="flex flex-col items-center text-center gap-6 w-full">
